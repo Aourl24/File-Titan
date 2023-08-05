4c7520765ea78ff8e86af3d8ae4a6b1e38002f83
Aourl24 <104075751+Aourl24@users.noreply.github.com>
2023-08-05 01:10:13 +0100

    Add files via upload

---
+++ FileShare/prod.py
@@ -0,0 +1,33 @@
+import environ
+import os
+
+
+env = environ.Env()
+environ.Env.read_env()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': env("HOST"),
+        'NAME': env("NAME"),
+        'USER': env("USER"),
+        'PASSWORD':env("PASSWORD"),
+        'PORT':env("PORT"),
+
+    }
+}
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME':env('CLOUD_NAME'),
+    'API_KEY':env('API_KEY'),
+    'API_SECRET':env('API_SECRET'), 
+}
+
+STORAGES = {'default':{"BACKEND":'cloudinary_storage.storage.MediaCloudinaryStorage'
+}, 
+"staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    }, 
+}
+
+
